ef1df10579d4ec67602aa2758293d1def700e16c
xianeml <xianemml@gmail.com>
2020-10-24 21:35:59 +0900

    1024 리액트 MRB - 포켓몬 카드 프로젝트

---
+++ React/TheModernReactBootcamp/PokemonProject/pokemon/src/Pokedex.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Pokecard from "./Pokecard";
+import "./Pokedex.css";
+
+const Pokedex = (props) => {
+  let title;
+  if (props.isWinner) {
+    title = <h1 className="Pokedex-winner">Winning Hand</h1>;
+  } else {
+    title = <h1 className="Pokedex-loser">Losing Hand</h1>;
+  }
+
+  return (
+    <div className="Pokedex">
+      {title}
+      <h4>Total Experience: {props.exp}</h4>
+      <div className="Pokedex-cards">
+        {props.pokemon.map((p) => (
+          <Pokecard
+            id={p.id}
+            name={p.name}
+            type={p.type}
+            exp={p.base_experience}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pokedex;
